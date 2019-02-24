86f5d098c70e2406fc55ef5696dadb693b461d69
Majosh <mjosh.dacasin@gmail.com>
2019-02-24 13:48:07 +0800

    first commifirst commit

---
+++ src/main/java/com/majosh/challenge/TechnicalTest.java
@@ -0,0 +1,10 @@
+package com.majosh.challenge;
+
+public class TechnicalTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/main/java/com/majosh/challenge/Xtable.java
@@ -0,0 +1,141 @@
+package com.majosh.challenge;
+
+
+public class Xtable {
+	private String columnA,
+	               columnB,
+	               columnC,
+	               columnD,
+	               columnE,
+	               columnF,
+	               columnG,
+	               columnH,
+	               columnI,
+	               columnJ;
+
+
+	public Xtable(String columnA, String columnB, String columnC, String columnD, String columnE, String columnF,
+			String columnG, String columnH, String columnI, String columnJ) {
+		super();
+		this.columnA = columnA;
+		this.columnB = columnB;
+		this.columnC = columnC;
+		this.columnD = columnD;
+		this.columnE = columnE;
+		this.columnF = columnF;
+		this.columnG = columnG;
+		this.columnH = columnH;
+		this.columnI = columnI;
+		this.columnJ = columnJ;
+	}
+
+
+	public Xtable() {
+		super();
+		
+	}
+
+
+	public String getColumnA() {
+		return columnA;
+	}
+
+
+	public void setColumnA(String columnA) {
+		this.columnA = columnA;
+	}
+
+
+	public String getColumnB() {
+		return columnB;
+	}
+
+
+	public void setColumnB(String columnB) {
+		this.columnB = columnB;
+	}
+
+
+	public String getColumnC() {
+		return columnC;
+	}
+
+
+	public void setColumnC(String columnC) {
+		this.columnC = columnC;
+	}
+
+
+	public String getColumnD() {
+		return columnD;
+	}
+
+
+	public void setColumnD(String columnD) {
+		this.columnD = columnD;
+	}
+
+
+	public String getColumnE() {
+		return columnE;
+	}
+
+
+	public void setColumnE(String columnE) {
+		this.columnE = columnE;
+	}
+
+
+	public String getColumnF() {
+		return columnF;
+	}
+
+
+	public void setColumnF(String columnF) {
+		this.columnF = columnF;
+	}
+
+
+	public String getColumnG() {
+		return columnG;
+	}
+
+
+	public void setColumnG(String columnG) {
+		this.columnG = columnG;
+	}
+
+
+	public String getColumnH() {
+		return columnH;
+	}
+
+
+	public void setColumnH(String columnH) {
+		this.columnH = columnH;
+	}
+
+
+	public String getColumnI() {
+		return columnI;
+	}
+
+
+	public void setColumnI(String columnI) {
+		this.columnI = columnI;
+	}
+
+
+	public String getColumnJ() {
+		return columnJ;
+	}
+
+
+	public void setColumnJ(String columnJ) {
+		this.columnJ = columnJ;
+	}
+	
+	
+	
+	
+}
